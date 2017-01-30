c841d8acb03e70807cb0a41e223d1ffcee6b1478
Luis Blanco <raubtierxxx@gmail.com>
2017-01-30 10:35:53 +0300

    fix

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ require('node-deps-qt-core-raub');
 const os = require('os');
 
 const addonPaths = {
-	win32 : '/bin_win32/webgl' ,
-	linux : '/bin_linux/webgl' ,
-	darwin: '/bin_darwin/webgl',
+	win32 : '/bin_win32' ,
+	linux : '/bin_linux' ,
+	darwin: '/bin_darwin',
 };
 
 process.env.path += ';' + __dirname + addonPaths[os.platform()];
