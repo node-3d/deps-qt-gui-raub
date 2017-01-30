41b9a51ea606ec8453cf701b8f2afe3745a984c9
Luis Blanco <raubtierxxx@gmail.com>
2017-01-30 09:36:13 +0300

    auto env

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
 
+const os = require('os');
+
+const addonPaths = {
+	win32 : '/bin_win32/webgl' ,
+	linux : '/bin_linux/webgl' ,
+	darwin: '/bin_darwin/webgl',
+};
+
+process.env.path += ';' + __dirname + addonPaths[os.platform()];
+
 module.exports = __dirname;
