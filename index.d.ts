9951236e287c88aeb99c2762e0bfc79ade1f8211
Luis Blanco <luisblanco1337@gmail.com>
2023-10-24 22:23:50 +0400

    Fix d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,21 @@
-declare module "deps-qt-core-raub" {
+declare module "deps-qt-gui-raub" {
+	/**
+	 * Exports of `deps-qt-core-raub`
+	*/
+	export const core: {
+		bin: string;
+		include: string;
+	};
+	
 	/**
 	 * Path to binaries
+	 *
 	 * Platform binary directory absolute path
 	*/
 	export const bin: string;
 	/**
 	 * Path to includes
+	 *
 	 * Include directory for this module
 	*/
 	export const include: string;
